2843477a1a6d3d226bc5fcd89c493197975ef095
Yaşar ARAZ <kodkopat@gmail.com>
2022-08-03 20:22:51 +0300

    Update Project structure

---
+++ Exchange/Exchange.API.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Exchange.API.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- Exchange/Exchange.Domain/Class1.cs
+++ Exchange/Exchange.Domain/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace Exchane.Domain
+﻿namespace Exchange.Domain
 {
     public class Class1
     {
